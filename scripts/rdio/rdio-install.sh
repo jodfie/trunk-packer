eca5dc141334ae68042a301dad9fa58071eeca81
Jody <jodfie@gmail.com>
2022-08-12 13:33:16 -0400

    finished install script v1

--- scripts/rdio/rdio-install.sh
+++ scripts/rdio/rdio-install.sh
@@ -6,4 +6,8 @@ mkdir rdio-scanner
 cd rdio-scanner
 
 #Download Rdio latest release
-curl -s https://api.github.com/repos/chuot/rdio-scanner/releases/latest | grep github.*rdio-scanner-linux-arm64-.*\.zip | cut -d : -f 2,3 | tr -d \"
+curl -s https://api.github.com/repos/chuot/rdio-scanner/releases/latest | grep github.*rdio-scanner-linux-arm64-.*\.zip | cut -d : -f 2,3 | tr -d \"| wget -i - -O rdio.zip
+
+#Unzip The Folder
+unzip rdio.zip
+mv rdio/* .
